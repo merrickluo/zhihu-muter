e271f9c3adae123845f9d684a8c9f1ea2e7ab8a5
ahxxm <ahxxm@users.noreply.github.com>
2017-01-30 20:11:01 +0800

    eslint (#12)
    * travis.yml: use jslint
    
    * use eslint instead
    
    * copy eslintrc from:
    
    https://gist.github.com/ghostwords/40936f11091b87987e56
    
    * remove lintrc initialization
    
    * eslintrc: remove duplicated mapping
    
    * eslint: enable --debug for verbose output
    
    * eslintrc: enable rules
    
    copied from: http://stackoverflow.com/questions/34644177/eslint-no-output
    
    * eslintrc: use json instead of deprecated format
    
    * eslint: add missing dep eslint-plugin-standard
    
    * eslint: remove unused promise plugin
    
    * eslintrc: fix json format, no trailing comma allowed
    
    * eslintrc: disable camel_case, allow 2 empty lines and semicolon
    
    * eslintrc: disable indent because long line
    
    * eslintrc: disable space-before-function-paren
    
    * style fixes: use === instead of ==, catch in same line with }
    
    * no-def fix: add 'var'

--- mute.js
+++ mute.js
@@ -22,12 +22,13 @@ var DEBUG = 1;
  * "upvoted THESE answers" ->  feed-item folding feed-item-hook feed-item-a combine.
  */
 
+
 // id and keyword list
-var mutelist = ["eric-ann", "yolfilm", "cogito", "gracelu", "lawrencelry", "nixy", "zi-mo-qi", 
-                "Fenng", "Hi-iD", "peng-yu-75-12", "Ratoo", "yang-guo-er-95", "xu-hui-lin", 
-                "mu-peng-37", "e6kq", "luvletter_", "ios_dev", "wang-wei-62-88", "stormzhang", 
-                "Hail_Hydra", "sunmengchao", "xing-tong-59-86"];
-var keywords = ["看待", "解读", "评价", "优雅", "如何", "评判", "却不", "体验", "但", "如何理解", "什么", "零基础", "鲜为人知", "特别急", "小众", "你", "有哪些", "现在", "孝", "冷知识", "中国", "阿姨", "刘仲敬", "工艺"];
+var mutelist = ['eric-ann', 'yolfilm', 'cogito', 'gracelu', 'lawrencelry', 'nixy', 'zi-mo-qi',
+                'Fenng', 'Hi-iD', 'peng-yu-75-12', 'Ratoo', 'yang-guo-er-95', 'xu-hui-lin',
+                'mu-peng-37', 'e6kq', 'luvletter_', 'ios_dev', 'wang-wei-62-88', 'stormzhang',
+                'Hail_Hydra', 'sunmengchao', 'xing-tong-59-86'];
+var keywords = ['看待', '解读', '评价', '优雅', '如何', '评判', '却不', '体验', '但', '如何理解', '什么', '零基础', '鲜为人知', '特别急', '小众', '你', '有哪些', '现在', '孝', '冷知识', '中国', '阿姨', '刘仲敬', '工艺'];
 
 var top_column_info_hidden = 1;
 var you_may_not_want_to_know = 1;
@@ -41,6 +42,7 @@ var relevent_questions = 1;
   document.getElementsByClassName("zu-top-nav-ul zg-clear")[0].appendChild(b);
 
 */
+
 
 var Magic = {
   authorInfo: 'zm-item-answer-author-info',
@@ -58,18 +60,16 @@ var Magic = {
 function hide(obj) {
   try {
     obj.style.display = 'none';
-  }
-  catch (err) {
+  } catch (err) {
     log(err);
   }
 }
 
 function log(msg) {
-  if (DEBUG == 1) {
+  if (DEBUG === 1) {
     console.log(msg);
   }
 }
-
 
 
 // 1. Answer
@@ -113,7 +113,6 @@ function comment_mute(className, tomute) {
       var muteword = tomute[j];
       if (e.getElementsByClassName(Magic.answerComment)[0].innerHTML.search(muteword) > -1) {
         hide(e);
-        //console.log(muteword)
       }
     }
   }
@@ -142,11 +141,11 @@ function mute_current_item(feed_item, keywords) {
 
   // upvote on answer
   var qtitles = feed_item.getElementsByClassName(Magic.timelineQlink);
-  if (qtitles.length == 1) {
+  if (qtitles.length === 1) {
     title = qtitles[0].innerHTML;
   } else {
     // upvote on article
-    var atitles = feed_item.getElementsByClassName("post-link");
+    var atitles = feed_item.getElementsByClassName('post-link');
     title = atitles[0].innerHTML;
   }
 
@@ -156,19 +155,17 @@ function mute_current_item(feed_item, keywords) {
       hide(feed_item);
     }
   }
-  return result;
 }
 
 function timeline_item_mute(keywords) {
   var elements = document.querySelectorAll(Magic.timelineFoldItem);
-  n = elements.length;
+  var n = elements.length;
   for (var i = 0; i < n; i++) {
     var feed_item = elements[i];
     try {
-      var result = mute_current_item(feed_item, keywords);
-    }
-    catch (err) {
-      // log('Not valid feed item: ' + err);
+      mute_current_item(feed_item, keywords);
+    } catch (err) {
+      log('Not valid feed item: ' + err);
     }
   }
 }
@@ -176,14 +173,14 @@ function timeline_item_mute(keywords) {
 // Main function
 
 // 0. Get current url to decide what to do
-hrefValue = window.location.href;
+var hrefValue = window.location.href;
 
 // 1. Static methods that do not require listeners
 // 1.1: People you may not want to know
-var httpHome = "http://www.zhihu.com/";
-var httpsHome = "https://www.zhihu.com/";
-if (hrefValue == httpHome || hrefValue == httpsHome) {
-  if (you_may_not_want_to_know == 1) {
+var httpHome = 'http://www.zhihu.com/';
+var httpsHome = 'https://www.zhihu.com/';
+if (hrefValue === httpHome || hrefValue === httpsHome) {
+  if (you_may_not_want_to_know === 1) {
     var column = document.getElementsByClassName('zh-trendings')[0];
     hide(column);
     log('No user suggestions anymore.');
@@ -192,8 +189,8 @@ if (hrefValue == httpHome || hrefValue == httpsHome) {
 
 // 1.2: Personal info and site log on the top
 // (hide for bigger -- hiths)
-var not_zhuanlan = (hrefValue.search("zhuanlan.zhihu") < 0);
-if (top_column_info_hidden == 1 && not_zhuanlan) {
+var not_zhuanlan = (hrefValue.search('zhuanlan.zhihu') < 0);
+if (top_column_info_hidden === 1 && not_zhuanlan) {
   var logo = document.getElementsByClassName(Magic.topLeftLogo)[0];
   hide(logo);
 
@@ -212,9 +209,8 @@ if (top_column_info_hidden == 1 && not_zhuanlan) {
 // 2. Methods that are not always used
 // 2.1 Question/Answer page
 if (hrefValue.search('question') > -1) {
-
   // Relevent question suggestion column
-  if (relevent_questions == 1) {
+  if (relevent_questions === 1) {
     var relevent = document.getElementsByClassName(Magic.releventQuestions);
     hide(relevent);
     log('No more suggestions on relevent questions.');
@@ -230,7 +226,7 @@ if (hrefValue.search('question') > -1) {
 }
 
 // 2.2 Timeline
-if (hrefValue.search('question') == -1) {
+if (hrefValue.search('question') === -1) {
   if (document.addEventListener) {
     document.addEventListener(Magic.event, function (event) {
       timeline_item_mute(keywords);
